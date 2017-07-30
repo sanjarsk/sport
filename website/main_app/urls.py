0c62c0df7bbbfdb5bbfa6391875bc9f89c1cb566
Sanzhar <sanjarkubanychbekov@gmail.com>
2017-07-30 17:21:26 +0600

    add table for team registering with 6 forms as a formset

--- website/main_app/urls.py
+++ website/main_app/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     url(r'^$', to_home, name='to_home'),
     url(r'^(?P<id>\d+)/$', show_active_game, name='show_active_game'),
-    url(r'^team-register$', ParticipantCreate.as_view(), name='team_register'),
+    url(r'^team-register$', create_team, name='team_register'),
 ]
